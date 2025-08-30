8ee58f8779afdbc50c5e08df1c62b271c897cca2
Rajarshi Mazumder <sapmazumder@gmail.com>
2025-08-31 00:57:22 +0900

    Deploy Angular frontend

--- frontend/food-analyzer-ui/src/environments/environment.ts
+++ frontend/food-analyzer-ui/src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  apiBase: 'http://127.0.0.1:5000', // Flask dev server
-  // apiBase: 'https://voluntary-arizona-attachments-stick.trycloudflare.com', // Flask dev server
+  // apiBase: 'http://127.0.0.1:5000', // Flask dev server
+  apiBase: 'https://langchain-rag-app-510514615062.asia-northeast1.run.app/',
 };
